b090b1fe7cdf59de7eb9e75bde69d56dfaf6bddb
Endzeitfee <52233973+Endzeitfee@users.noreply.github.com>
2022-01-30 13:23:11 +0100

    Add files via upload

--- 100DaysOfCode-JS-2/main.js
+++ 100DaysOfCode-JS-2/main.js
@@ -43,3 +43,18 @@ console.dir(paragraphElement);
 //    you selected in (4) and set it to any other text of your choice
 
 paragraphElement = textContent = "This is the end of the world as we know it.";
+
+// Add an element
+//1. Create the new element.
+
+let newAnchorElement = document.createElement("a");
+newAnchorElement.href = "https://google.com";
+newAnchorElement.textContent = "This leads to google.";
+
+//2. Get access to the parent element that should hold the new element.
+
+let firstParagraph = document.querySelector("p");
+
+//3. Insert the new element into the parent element content.
+
+firstParagraph.append(newAnchorElement);
